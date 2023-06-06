dbb90ea6ad8a575460813e43bd2b9115b0c9b768
Alejik <etoakame322@yahoo.com>
2023-06-06 19:40:25 +0300

    added 10-Interpreter pattern

---
+++ 10-Interpreter pattern/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+// Abstract expression
+public abstract class Expression
+{
+    public abstract int Interpret();
+}
+
+// Terminal expression
+public class NumberExpression : Expression
+{
+    private int _number;
+
+    public NumberExpression(int number)
+    {
+        _number = number;
+    }
+
+    public override int Interpret()
+    {
+        return _number;
+    }
+}
+
+// Non-terminal expression
+public class AdditionExpression : Expression
+{
+    private Expression _leftExpression;
+    private Expression _rightExpression;
+
+    public AdditionExpression(Expression leftExpression, Expression rightExpression)
+    {
+        _leftExpression = leftExpression;
+        _rightExpression = rightExpression;
+    }
+
+    public override int Interpret()
+    {
+        return _leftExpression.Interpret() + _rightExpression.Interpret();
+    }
+}
+
+// Non-terminal expression
+public class SubtractionExpression : Expression
+{
+    private Expression _leftExpression;
+    private Expression _rightExpression;
+
+    public SubtractionExpression(Expression leftExpression, Expression rightExpression)
+    {
+        _leftExpression = leftExpression;
+        _rightExpression = rightExpression;
+    }
+
+    public override int Interpret()
+    {
+        return _leftExpression.Interpret() - _rightExpression.Interpret();
+    }
+}
+
+// Client
+public class Client
+{
+    public static void Main()
+    {
+        // Create the expression tree: 5 + (2 - 3)
+        Expression expression = new AdditionExpression(
+            new NumberExpression(5),
+            new SubtractionExpression(
+                new NumberExpression(2),
+                new NumberExpression(3)
+            )
+        );
+
+        // Interpret the expression
+        int result = expression.Interpret();
+        Console.WriteLine("Result: " + result); // Output: Result: 4
+    }
+}
