f078e6b91407dbf60eca31e893e408b0de47acc2
Alejik <etoakame322@yahoo.com>
2023-06-06 21:52:39 +0300

    temporary deleted test for 9-BlackBoard pattern due my stupidity

--- 9-Blackboard pattern/BlackBoard.cs
+++ 9-Blackboard pattern/BlackBoard.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 class Blackboard
 {
     public string Data { get; set; }
--- 9-Blackboard pattern/Client.cs
+++ 9-Blackboard pattern/Client.cs
@@ -1,3 +1,5 @@
+using System;
+
 class Client
 {
     private Blackboard _blackboard;
